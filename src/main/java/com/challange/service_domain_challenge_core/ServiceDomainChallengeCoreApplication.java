5654ddac63dacb11667a98606a986b483b4e17a9
noah-wilson0 <127368908+noah-wilson0@users.noreply.github.com>
2026-06-26 01:33:52 +0900

    first commit

---
+++ src/main/java/com/challange/service_domain_challenge_core/ServiceDomainChallengeCoreApplication.java
@@ -0,0 +1,13 @@
+package com.challange.service_domain_challenge_core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceDomainChallengeCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceDomainChallengeCoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/challange/service_domain_challenge_core/ServiceDomainChallengeCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.challange.service_domain_challenge_core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceDomainChallengeCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
